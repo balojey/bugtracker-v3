29f07928241218bc0e53aa19272780eeebeaa981
Ademola Balogun <balotech514@gmail.com>
2023-08-06 19:44:58 +0100

    Created login page

---
+++ bugtracker-app/bugtracker_app/pages/login.py
@@ -0,0 +1,22 @@
+from ..components.components import demo_user_login, user_login, header
+import reflex as rx
+
+
+def login_page() -> rx.Component:
+    """Login Page"""
+    return rx.box(
+        header(),
+        rx.heading("Login", size="lg"),
+        rx.responsive_grid(
+            user_login(flex_grow="1", padding_x="1em",),
+            demo_user_login(flex_grow="1", padding_x="1em",),
+            columns=[2],
+            spacing="8",
+        ),
+        width="100vw",
+        height="100vh",
+        display="flex",
+        justify_content="center",
+        align_items="stretch",
+        flex_direction="column",
+    )
