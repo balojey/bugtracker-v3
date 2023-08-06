f1b76de985556a63bc48af518b3b3df66db6ab79
Ademola Balogun <balotech514@gmail.com>
2023-08-06 19:44:17 +0100

    Added style for all button and headings

---
+++ bugtracker-app/bugtracker_app/styles/base_styles.py
@@ -0,0 +1,12 @@
+import reflex as rx
+
+styles = {
+    rx.Button: {
+        "border": "0.1em solid",
+        "border_radius": "0",
+        "padding": "0.5em",
+    },
+    rx.Heading: {
+        "margin": "0.5em 0",
+    }
+}
