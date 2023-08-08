f5315914fa00ed1dacd0138e57945f1cf55ecb2f
Ademola Balogun <balotech514@gmail.com>
2023-08-08 02:09:13 +0100

    Added computed vars that checks the role of the logged in user

--- bugtracker-app/bugtracker_app/states/base.py
+++ bugtracker-app/bugtracker_app/states/base.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from ..models import User
+from ..enumerations import Role
 from typing import Optional
 
 
@@ -7,6 +8,26 @@ class State(rx.State):
     """The base state for the app"""
 
     user: Optional[User] = None
+
+    @rx.var
+    def is_admin(self):
+        """Check if user is admin"""
+        return self.user.role == Role.ADMIN
+
+    @rx.var
+    def is_project_manager(self):
+        """Check if user is project manager"""
+        return self.user.role == Role.PROJECT_MANAGER
+
+    @rx.var
+    def is_developer(self):
+        """Check if user is developer"""
+        return self.user.role == Role.DEVELOPER
+
+    @rx.var
+    def is_submitter(self):
+        """Check if user is submitter"""
+        return self.user.role == Role.SUBMITTER
 
     def logout(self):
         """Logout user"""
