7261ce55ae05b672d359835b9d5b1c55e81cae85
Ademola Balogun <balotech514@gmail.com>
2023-12-23 08:11:04 +0100

    Refurbishing

--- frontend/frontend/schemas/response_status.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ResponseStatus(str, Enum):
-    SUCCESS = "success"
-    ERROR = "error"
--- frontend/frontend/schemas/status.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class BugStatus(int, Enum):
-    """Bug status enum"""
-    NEW = 1
-    IN_PROGRESS = 2
-    RESOLVED = 3
-    CLOSED = 4
